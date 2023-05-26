d2b20836b7fbda8d1bd61a8420a0c026c2e5b28d
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-05-26 20:12:09 +0300

    Fix linters

--- solver.rb
+++ solver.rb
@@ -1,17 +1,17 @@
 class Solver
-    def factorial(num)
-        if num < 0
-            raise ArgumentError, 'Input must be a positive number'
-        else
-            result = 1
-            (1..num).each do |i|
-                result *= i
-            end
-            result
-        end
+  def factorial(num)
+    if num < 0
+      raise ArgumentError, 'Input must be a positive number'
+    else
+      result = 1
+      (1..num).each do |i|
+        result *= i
+      end
+      result
     end
+  end
 
-    def reverse(word)
-        word.reverse
-    end
+  def reverse(word)
+    word.reverse
+  end
 end
