a74ceefa1ca5f34632122c559880ff3edd4cf75a
edubew <edubew98@gmail.com>
2023-05-26 14:45:57 +0300

    Write a test for the factorial method

--- solver_spec.rb
+++ solver_spec.rb
@@ -10,3 +10,17 @@ describe Solver do
         expect(@solver).to be_an_instance_of Solver
     end
 end
+
+describe '#factorial' do
+it 'returns the factorial of the number' do
+    expect(@solver.factorial(4)).to eq(24)
+end
+
+it 'returns 1 if the factorial method argument is 0' do
+    expect(@solver.factorial(0)).to eq(1)
+end
+
+it 'raise an argument error' do
+    { @solver.factorial(-1) }.to raise_error(ArgumentError, 'Input must be a positive number!')
+end
+end
